2d5fc9b44df666a230a2ca70802da23d1a037cc6
Pramod Jingade <pramod.jingade@gmail.com>
2019-09-16 02:42:24 +0530

    added favicons

--- backend/build/routes/app.routes.js
+++ backend/build/routes/app.routes.js
@@ -1,12 +1,27 @@
-"use strict";
+'use strict';
 
 Object.defineProperty(exports, "__esModule", {
-  value: true
+    value: true
 });
+
+var _session = require('./../controllers/session.controller');
+
+var _flights = require('./../controllers/flights.controller');
+
 /**
  * Main Webservice Routes application
 */
-var routes = function routes(app) {};
+var sessionReq = new _session.SessionController();
+var flights = new _flights.FlightsController();
+
+var routes = function routes(app) {
+
+    // SESSION=====
+    app.route('/session').post(sessionReq.createSession);
+
+    // BROWSE FLIGHTS=====
+    app.route('/flights').post(flights.browseFlights);
+};
 
 exports.default = routes;
 //# sourceMappingURL=app.routes.js.map
